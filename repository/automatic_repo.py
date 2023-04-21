254550df8cac06cfb1ee0efdff7fa4fc5ab02b59
skazal15 <khan.said86@gmail.com>
2023-04-21 07:39:32 +0700

    add board endpoint

--- repository/automatic_repo.py
+++ repository/automatic_repo.py
@@ -6,31 +6,37 @@ from util.logger import logger
 class Automatic:
     def getAllData():
         print('get all')
+        session = db_session
         try:
             automatic = TabelKontrol.query.all()
             logger.info(automatic)
             print(automatic)
             return automatic
         except SQLAlchemyError as e:
-            db_session.rollback()
+            session.rollback()
             logger.info(e)
         finally:
-            db_session.remove()
+            session.remove()
     
     def getSpecificBoardData(board):
+        session = db_session
         try:
             automatic = TabelKontrol.query.filter_by(board = board)
             return automatic
+        except SQLAlchemyError as e:
+            session.rollback()
+            logger.info(e)
         finally:
-            db_session.remove()
+            session.remove()
 
     def setState(board,gpio,state):
+        session = db_session
         try:
             automatic = TabelKontrol.query.filter_by(board = board,gpio = gpio)
             automatic.state = state
-            db_session.commit()
+            session.commit()
         except SQLAlchemyError as e:
-            db_session.rollback()
+            session.rollback()
             logger.info(e)
         finally:
-            db_session.remove()
+            session.remove()
